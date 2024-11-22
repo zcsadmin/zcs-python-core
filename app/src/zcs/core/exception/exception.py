055fd213f056e60dc10da1cc87af49b0f056ec59
Michele Mondelli <m.mondelli@zcscompany.com>
2024-11-22 16:12:41 +0100

    feat: add detailed information to cloud json formatter

--- app/src/zcs/core/exception/exception.py
+++ app/src/zcs/core/exception/exception.py
@@ -1,9 +1,10 @@
 import random
 import string
+import traceback
 
 class ZcsException(Exception):
 
-    def __init__(self, user_message, error_source = "Unspecified", status_code = 500, internal_message = None):
+    def __init__(self, user_message = "Unknown error.", error_source = "Unspecified", status_code = 500, internal_message = None):
         super().__init__(user_message)
         
         self.__error_source = error_source
@@ -33,5 +34,9 @@ class ZcsException(Exception):
             'user_message': self.get_user_message(),
             'internal_message': self.get_internal_message(),
             'error_source': self.get_error_source(),
-            'status_code': self.get_status_code()
+            'status_code': self.get_status_code(),
+            'traceback': "".join(traceback.format_exception(self))
         }
+    
+    def __str__(self):
+        return f"{self.get_error_code()} - {self.get_error_source()} - {self.get_internal_message()} - {self.get_user_message()}"
