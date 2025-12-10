0ee015886549e0ac3accf94c40301d8950dd9368
Andrea Marchetti <a.marchetti@zcscompany.com>
2025-12-10 11:14:28 +0100

    add qa_info

--- app/src/zcs/core/session/__init__.py
+++ app/src/zcs/core/session/__init__.py
@@ -2,3 +2,4 @@ from .request_context import request_context
 from .request_state import RequestState
 from .auth_info import AuthInfo
 from .follia_module import FolliaModule
+from .qa_info import QaInfo
